bc0be53fbae2871973d312fb6ebfd3240e80c6b7
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-21 22:29:02 +0600

    use strategy pattern in encryption mode. create interface and mode class

--- AES-Application/AES.Encryption/Mode/CBCMode.cs
+++ AES-Application/AES.Encryption/Mode/CBCMode.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AES.Encryption
+namespace AES.Encryption.Mode
 {
-    public class Class1
+    public class CBCMode
     {
     }
 }
