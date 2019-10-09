5319f731372e0f4a838add75acc93dcead0c0fb1
khayrulislam <bsse0822@iit.du.ac.bd>
2019-10-09 11:05:08 +0600

    remove all the string path constant

--- AES-Application/AES.EncryptDecrypt/Utility/Constants.cs
+++ AES-Application/AES.EncryptDecrypt/Utility/Constants.cs
@@ -8,12 +8,6 @@
         public static int MIX_COLUMN_CONSTANT = 27;
         public static int INPUT_BLOCK_SIZE = 16;
 
-
-        public static string S_BOX_FILE_PATH = "../../../AES.EncryptDecrypt/s-Box/s-box.txt";
-        public static string INVERSE_S_BOX_FILE_PATH = "../../../AES.EncryptDecrypt/s-Box/inverse-s-box.txt";
-        public static string MIX_COLUMN_FILE_PATH = "../../../AES.EncryptDecrypt/mixColumn/mix-column-constant.txt";
-        public static string INVERSE_MIX_COLUMN_FILE_PATH = "../../../AES.EncryptDecrypt/mixColumn/inverse-mix-column-constant.txt";
-        public static string KEY_CONSTANT_FILE_PATH = "../../../AES.EncryptDecrypt/KeyExpand/key-constant.txt";
         public static string INPUT_FILE_PATH = "../../../AES.EncryptDecrypt/fileReader/input.txt";
         public static string INPUT_FILE_PATH2 = "../../../AES.EncryptDecrypt/fileReader/input.pdf";
         public static string OUTPUT_FILE_PATH = "../../../AES.EncryptDecrypt/fileReader";
